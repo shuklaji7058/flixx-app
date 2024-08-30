02778d248e85b49b73359b5fcf710984769e534c
Anish Shukla <shuklaanish040@gmail.com>
2024-08-30 13:14:26 +0530

    initial commit and theme

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+//Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+//Init App
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home");
+      break;
+    case "shows.html":
+      console.log("Shows");
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+  highlightActiveLink();
+}
+
+document.addEventListener("DOMContentLoaded", init);
